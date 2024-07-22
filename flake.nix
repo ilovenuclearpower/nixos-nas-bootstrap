294b3609428fb0801deaf29c75b3b48c86362567
hik <throwawayforservices1689@aboveaverage.space>
2024-07-22 02:16:57 +0000

    Added users

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
   outputs = { self, nixpkgs }: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
-      modules = [ ./configuration.nix ];
+      modules = [ ./configuration.nix ./users.nix ];
     };
   };
 }
